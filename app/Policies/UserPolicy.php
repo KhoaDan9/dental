3590d88f7c68b96a8bbda0cbdb4c690994af8062
KhoaDan9 <khoadangx2@gmail.com>
2025-10-19 23:09:45 +0700

    Update logo + view

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -9,7 +9,7 @@ class UserPolicy
 {
     public function viewAny(User $user): bool
     {
-        if ($user->admin == 1 || $user->accessControls()->where('feature_id',37)->where('permission_id',1)->first()->user_permission == '1') {
+        if ($user->admin == 1 || $user->accessControls()->where('feature_id', 37)->where('permission_id', 1)->first()->user_permission == '1') {
             return true;
         }
         return false;
@@ -18,15 +18,18 @@ class UserPolicy
 
     public function view(User $user, User $model): bool
     {
-        if ($user->admin == 1 || $user->accessControls()->where('feature_id',37)->where('permission_id',1)->first()->user_permission == '1') {
+        if ($user->admin == 1) {
+            return true;
+        } elseif ($user->accessControls()->where('feature_id', 37)->where('permission_id', 1)->first()->user_permission == '1') {
             return $model->id == $user->id;
+
         }
         return false;
     }
 
     public function create(User $user): bool
     {
-        if ($user->admin == 1 || $user->accessControls()->where('feature_id',37)->where('permission_id',2)->first()->user_permission == '1') {
+        if ($user->admin == 1 || $user->accessControls()->where('feature_id', 37)->where('permission_id', 2)->first()->user_permission == '1') {
             return true;
         }
         return false;
@@ -34,7 +37,7 @@ class UserPolicy
 
     public function update(User $user): bool
     {
-        if ($user->admin == 1 || $user->accessControls()->where('feature_id',37)->where('permission_id',3)->first()->user_permission == '1') {
+        if ($user->admin == 1 || $user->accessControls()->where('feature_id', 37)->where('permission_id', 3)->first()->user_permission == '1') {
             return true;
         }
         return false;
@@ -42,7 +45,7 @@ class UserPolicy
 
     public function delete(User $user): bool
     {
-        if ($user->admin == 1 || $user->accessControls()->where('feature_id',37)->where('permission_id',4)->first()->user_permission == '1') {
+        if ($user->admin == 1 || $user->accessControls()->where('feature_id', 37)->where('permission_id', 4)->first()->user_permission == '1') {
             return true;
         }
         return false;
@@ -51,7 +54,7 @@ class UserPolicy
     // cấp quyền đổi mật khẩu
     public function viewAccount(User $user): bool
     {
-        if ($user->admin == 1 || $user->accessControls()->where('feature_id',2)->where('permission_id',1)->first()->user_permission == '1') {
+        if ($user->admin == 1 || $user->accessControls()->where('feature_id', 2)->where('permission_id', 1)->first()->user_permission == '1') {
             return true;
         }
         return false;
@@ -59,7 +62,7 @@ class UserPolicy
 
     public function viewDataLogs(User $user): bool
     {
-        if ($user->admin == 1 || $user->accessControls()->where('feature_id',38)->where('permission_id',1)->first()->user_permission == '1') {
+        if ($user->admin == 1 || $user->accessControls()->where('feature_id', 38)->where('permission_id', 1)->first()->user_permission == '1') {
             return true;
         }
         return false;
--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,25 +1,31 @@
 <div class="fixed inset-0 flex items-center justify-center z-50 bg-gray-500">
     <div class="flex flex-col">
         <form wire:submit='login'>
-            <div class="bg-white rounded-lg shadow-lg w-full flex flex-col justify-center h-100 px-20">
-                <p>Tên đăng nhập</p>
-                <input class="px-1 border-gray-500 border-1 py-1 w-100" type="text" wire:model='username'>
-                @if ($errorMessage !== '')
-                    <x-error-message>{{ $errorMessage }}</x-error-message>
-                @endif
+            <div class="bg-white rounded-lg shadow-lg w-full flex flex-col h-80 px-15 py-10">
+                <div class="flex justify-center pb-5">
+                    <img src="{{Storage::url('/photos/Logo-black.png')}}" class="w-40" alt="">
+                </div>
                 <div>
-                    @error('username')
+                    <p>Tên đăng nhập</p>
+                    <input class="px-1 border-gray-500 border-1 py-1 w-100" type="text" wire:model='username'>
+                    @if ($errorMessage !== '')
+                        <x-error-message>{{ $errorMessage }}</x-error-message>
+                    @endif
+                    <div>
+                        @error('username')
                         <x-error-message>{{ $message }}</x-error-message>
-                    @enderror
-                </div>
-                <p>Mật khẩu</p>
-                <input class="px-1 border-gray-500 border-1 py-1  w-100" type="text" wire:model='password'>
-                 <div>
-                    @error('password')
+                        @enderror
+                    </div>
+                    <p>Mật khẩu</p>
+                    <input class="px-1 border-gray-500 border-1 py-1  w-100" type="password" wire:model='password'>
+                    <div>
+                        @error('password')
                         <x-error-message>{{ $message }}</x-error-message>
-                    @enderror
+                        @enderror
+                    </div>
+                    <button type="submit" class="!w-full main-button my-2 py-1" >Đăng nhập</button>
                 </div>
-                <button type="submit" class="!w-full main-button my-2 py-1">Đăng nhập</button>
+
             </div>
         </form>
     </div>
