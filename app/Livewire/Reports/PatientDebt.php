44ba5966279f933d6c05d3a77c85918e5056fbec
KhoaDan9 <khoadangx2@gmail.com>
2025-11-04 09:37:25 +0700

    update sum

--- app/Livewire/Reports/PatientDebt.php
+++ app/Livewire/Reports/PatientDebt.php
@@ -15,20 +15,29 @@ class PatientDebt extends Component
     {
         $patient_payments = PatientPayment::get()->groupBy('patient_id')->map(fn($g) => $g->sum('paid'));
         $patient_services = PatientService::get()->groupBy('patient_id')->map(fn($g) => $g->sum('total_price'));
+
         $patient_list = $patient_services->map(function ($total_price, $patient_id) use ($patient_payments) {
             $paid = $patient_payments[$patient_id] ?? 0;
             $debt = $total_price - $paid;
+
             return $debt > 0 ? $debt : null;
         })->filter();
 
+
         $patients = Patient::whereIn('id', $patient_list->keys())->get();
-        $this->data_list = $patients->map(function ($patient) use ($patient_list) {
+        $this->data_list = $patients->map(function ($patient) use ($patient_list, $patient_services, $patient_payments) {
             $updated_at = PatientService::where('id', $patient->id)->latest('updated_at')->first('updated_at');
+            $patient['total_price'] = $patient_services[$patient->id];
+            $patient['paid'] = $patient_payments[$patient->id] ?? 0;
             $patient['debt'] = $patient_list[$patient->id];
             $patient['updated_at'] = $updated_at ? $updated_at->updated_at : null;
             return $patient;
         });
+
         $this->data_list = $this->data_list->sortBy('updated_at');
+        $this->data_list->sum_total_price = $this->data_list->sum('total_price');
+        $this->data_list->sum_paid = $this->data_list->sum('paid');
+        $this->data_list->sum_debt = $this->data_list->sum('debt');
     }
 
     public function render()
--- resources/views/livewire/reports/patient-debt.blade.php
+++ resources/views/livewire/reports/patient-debt.blade.php
@@ -15,6 +15,8 @@
                 <th class="whitespace-nowrap w-0 text-left">Địa chỉ</th>
                 <th class="whitespace-nowrap w-0 text-left">Điện thoại</th>
                 <th class="whitespace-nowrap w-0">Lần cuối</th>
+                <th class="whitespace-nowrap w-0">Phải trả</th>
+                <th class="whitespace-nowrap w-0">Đã thu</th>
                 <th class="whitespace-nowrap w-0">Còn nợ</th>
             </tr>
             @foreach ($data_list as $data)
@@ -25,9 +27,18 @@
                     <td>{{ $data->address }}</td>
                     <td>{{ $data->phone }}</td>
                     <td class="text-center whitespace-nowrap">{{ \Carbon\Carbon::parse($data->updated_at)->format('d/m/Y') }}</td>
+                    <td class="text-right">{{ number_format($data->total_price , 0, ',', '.') }}</td>
+                    <td class="text-right">{{ number_format($data->paid , 0, ',', '.') }}</td>
                     <td class="text-right">{{ number_format($data->debt , 0, ',', '.') }}</td>
                 </tr>
             @endforeach
+            <tr>
+                <td colspan="6" class="text-right sum-td">Tổng cộng</td>
+                <td class="text-right sum-td">{{ number_format($data_list->sum_total_price , 0, ',', '.') }}</td>
+                <td class="text-right sum-td">{{ number_format($data_list->sum_paid , 0, ',', '.') }}</td>
+                <td class="text-right sum-td">{{ number_format($data_list->sum_debt , 0, ',', '.') }}</td>
+
+            </tr>
         </table>
     @endcannot
 
